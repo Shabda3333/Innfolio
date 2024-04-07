edef7585223631409d8497cd7ea0861526ac8f2e
Aashish Kadel <kadelaashish22@gmail.com>
2024-04-08 00:52:50 +0545

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-black text-white'>Hello this is shdaba</h1>
+      <h1 className='bg-black text-white font-bold'>Hello this is shabda. Now it is aashish.</h1>
+      <h1 className='bg-red-500 text-white font-bold'>Hello this is shabda. Now it is aashish.</h1>
+      <h1 className='bg-blue-400 text-white font-bold'>Hello this is shabda. Now it is aashish.</h1>
     </>
   )
 }
