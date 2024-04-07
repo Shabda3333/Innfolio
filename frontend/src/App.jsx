440aa0e433f688975eeb39cf5927ca4d9f2a086d
Aashish Kadel <kadelaashish22@gmail.com>
2024-04-08 00:55:19 +0545

    frontend initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-black text-white'>Hello this is shdaba</h1>
+      <h1 className='bg-black text-white'>Hello this is shabda. someone else is here too.</h1>
     </>
   )
 }
