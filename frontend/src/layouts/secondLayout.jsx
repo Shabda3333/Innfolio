fcdf2a30353ea23a618b299e818456f54f6fab34
Aashish Kadel <kadelaashish22@gmail.com>
2024-04-09 21:39:17 +0545

    fixed overflow issue and finished styling homepage

---
+++ frontend/src/layouts/secondLayout.jsx
@@ -0,0 +1,20 @@
+import InnfolioLogo from "../assets/innfolio.svg";
+
+const secondLayout = ({ children }) => {
+  return (
+    <div className="bg-[#141414] w-full h-screen overflow-hidden">
+      <div className="relative">
+        <div className="top-gradient w-[58rem] h-[58rem] absolute top-[-610px] left-[-270px] rounded-full gradient-one z-10 blur-[150px] opacity-40"></div>
+        <div className="bottom-gradient w-[58rem] h-[58rem] rotate-[56.83deg] absolute bottom-[-1400px] right-[-530px] rounded-full gradient-one z-10 blur-[150px] opacity-40"></div>
+        <img
+          src={InnfolioLogo}
+          alt="logo"
+          className="absolute top-[3.625rem] left-[7.5rem]"
+        />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default secondLayout;
--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,9 +1,44 @@
 // import React from 'react'
+import SecondLayout from "../layouts/secondLayout";
+import BgShapes1 from "../assets/illustrations/bg_shapes_1.svg";
+import BgShapes2 from "../assets/illustrations/bg_shapes_2.svg";
+import Laptop from "../assets/illustrations/laptop.svg";
+import MobileMic from "../assets/illustrations/mobile_mic.svg";
+import LoginSignupButton from "../components/LoginSignupButton";
+import {Link} from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>Welcome to Innfolio. Do you want to login or signup?</div>
-  )
-}
+    <div className="wrapper">
+      <SecondLayout>
+        <div className="center-content position-center w-[53rem] flex flex-col justify-center items-center gap-20">
+          <div className="text flex flex-col gap-8 justify-center items-center">
+            <p className="text-white text-3xl font-regular">Welcome to <span className="text-cyan">Innfolio</span></p>
+            <h3 className="text-white text-7xl font-semibold text-center">Unlock Your Professional Brilliance</h3>
+            <p className="text-cyan text-4xl font-medium">Craft <span className="text-white font-regular">Portfolio of Excellence</span></p>
+          </div>
+          <div className="call-to-actions w-full flex justify-center items-center gap-8">
+            <Link to = "/signin">
+              <LoginSignupButton title = "Sign In"/>
+            </Link>
+            <Link to = "/signup">
+              <LoginSignupButton title = "Sign Up"/>
+            </Link>
+          </div>
+        </div>
+        <div className="background-illustrations relative w-full">
+          <img src={BgShapes1} alt="abstract shapes" className="fixed top-[275px]" />
+          <img src={BgShapes2} alt="abstract shapes" className="fixed right-0 top-[600px]" />
+          <img src={MobileMic} alt="mobile mic illustration" className="fixed top-[-46px] right-0" />
+          <img
+            src={Laptop}
+            alt="laptop illustration"
+            className="fixed bottom-0 left-0"
+          />
+        </div>
+      </SecondLayout>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
