2da375a43ff2ae7e592940bacfcbd9dc7fbe160d
Aashish Kadel <kadelaashish22@gmail.com>
2024-04-09 19:43:35 +0545

    changed layout margin to padding

--- frontend/src/layouts/mainLayout.jsx
+++ frontend/src/layouts/mainLayout.jsx
@@ -2,7 +2,7 @@
 
 const mainLayout = ({children}) => {
   return (
-    <div className='mx-[7.5rem] z-20 pt-[10rem] pb-[5rem]'>{children}</div>
+    <div className='px-[7.5rem] z-20 pt-[10rem] pb-[5rem]'>{children}</div>
   )
 }
 
