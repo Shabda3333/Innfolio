3e11e52be407ef3c93ec0118f5c46cadc22c1b95
Shabda Shrestha <shabdashrestha3@gmail.com>
2024-04-10 07:33:00 +0545

    Changes in Homepage

--- frontend/src/layouts/mainLayout.jsx
+++ frontend/src/layouts/mainLayout.jsx
@@ -2,11 +2,7 @@
 
 const MainLayout = ({children}) => {
   return (
-<<<<<<< HEAD
-    <div className='px-[120px] py-[40px] bg-[url("./src/assets/welcomepagebg.svg")] h-[100vh] bg-cover bg-center  '>{children}</div>
-=======
     <div className='px-[7.5rem] z-20 pt-[10rem] pb-[5rem]'>{children}</div>
->>>>>>> 2da375a43ff2ae7e592940bacfcbd9dc7fbe160d
   )
 }
 
--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-
-// import React from "react";
-import MainLayout from "../layouts/mainLayout";
-
-
-const Homepage = () => {
-  return (
-    <MainLayout>
-      <div>
-        <img src="./src/assets/innfolio..svg" alt="" />
-      </div>
-
-      <div className=" bg-transparent text-white font-general-sans h-[80vh] flex flex-col justify-center items-center ">
-        <h2 className=" font-thin text-2xl">
-          Welcome, to <span className="text-cyan">Innfolio</span>
-        </h2>
-        <h1 className=" font-semibold text-7xl pt-[32px] pd-[10px]">
-          Unlock Your
-        </h1>
-        <h1 className=" font-semibold text-7xl pt-[10px] pd-[32px]">
-          Professional Brilliance
-        </h1>
-        <h2 className=" font-medium text-5xl pt-[32px] ">
-          <span className="text-cyan font-semibold">Craft</span> Portfolio of
-          Excellence
-        </h2>
-      </div>
-     
-    </MainLayout>
-  );
-};
-
-=======
 // import React from 'react'
 import SecondLayout from "../layouts/secondLayout";
 import BgShapes1 from "../assets/illustrations/bg_shapes_1.svg";
@@ -75,5 +41,4 @@ const Homepage = () => {
   );
 };
 
->>>>>>> fcdf2a30353ea23a618b299e818456f54f6fab34
 export default Homepage;
